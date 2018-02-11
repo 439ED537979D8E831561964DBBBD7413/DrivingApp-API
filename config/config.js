808685563c55bbfa697ab12c838da50fb318f915
danieltoo <torresestradadaniel@gmail.com>
2018-02-10 23:40:54 -0600

    Actualicé versión de ngsi-parser

--- config/config.js
+++ config/config.js
@@ -3,10 +3,10 @@ module.exports = {
     'secret': 'Sdk.fiware2017',  //secret key for our JSON Web Token.
     'fcm': 'AAAA2fXAP38:APA91bFVCZIUNE3oGxmFU0elSWGLPMLtNZ7dV52B1g5dei3wFP0yIBsjW48jw2LpigziN42Jsb-2ia4_NRyXw_LVD-epM1PSBjOD2ms2Ch7Wkxka-YwUBX-QD8wCmlm0120ue3HyMGHb',
     //MLAB MONGODB CONNECTION
-    'database': 'mongodb://admin:admin@ds125195.mlab.com:25195/smartsecurityfdm'
+    //'database': 'mongodb://admin:admin@ds125195.mlab.com:25195/smartsecurityfdm'
     //MONGODB LOCALHOST CONNECTION
     //'database':'mongodb://localhost:27017/smartsecurityfdm'
     //DOCKER CONTAINER (FIWARE INSTANCE 207.249.127.218) CONNECTION 
-    //'database':'mongodb://mongo:27017/smartsecurityfdm'
+    'database':'mongodb://mongo:27017/smartsecurityfdm'
 
 };
