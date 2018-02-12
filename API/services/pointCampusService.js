c2aa56edeb78600d1008137de3a2cedd91f5eafa
danieltoo <torresestradadaniel@gmail.com>
2018-02-12 17:17:11 -0600

    Cambié PointCampus Service

--- API/services/pointCampusService.js
+++ API/services/pointCampusService.js
@@ -2,9 +2,22 @@ var PointOnCampus = require('../../NotificationsServer/functions/pointOnCampus')
 
 exports.pointCampus= function (req,res) {
     let body = req.body;
-    console.log(body);
+
     let point = body.point.split(",");
-    let polygon = body.polygon.split(";").split(",");
+    point[0] = Number(point[0])
+    point[1] = Number(point[1])
+
+    //console.log(point)
+    let polygon = body.polygon.split(";");
+    for(let i in polygon){
+        tempPolygon = polygon[i].split(",")
+        tempPolygon[0] = Number(tempPolygon[0]);
+        tempPolygon[1] = Number(tempPolygon[1]);
+        polygon[i] = tempPolygon;
+    }
+
+    //console.log(polygon)
+    
     let salida = PointOnCampus(point, polygon);
     res.json({ isOnCampus: salida});
 }
