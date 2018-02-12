545eb395894e5752b2d5d5d334e61a982f2e5ffe
danieltoo <torresestradadaniel@gmail.com>
2018-02-12 17:02:19 -0600

    Cambié mensajes

--- API/services/pointCampusService.js
+++ API/services/pointCampusService.js
@@ -1,9 +1,10 @@
 var PointOnCampus = require('../../NotificationsServer/functions/pointOnCampus');
 
 exports.pointCampus= function (req,res) {
+    let body = req.body;
     console.log(body);
-    let point = body.point.split(";");
-    let polygon = body.polygon.split(";");
+    let point = body.point.split(",");
+    let polygon = body.polygon.split(";").split(",");
     let salida = PointOnCampus(point, polygon);
     res.json({ isOnCampus: salida});
 }
