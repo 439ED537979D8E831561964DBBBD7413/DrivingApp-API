1d7bcbb149fdd03ad9788270fd470a506330cd3f
danieltoo <torresestradadaniel@gmail.com>
2018-03-02 20:22:14 -0600

    Evita error de no encontrar devices

--- NotificationsServer/functions/getDevicesTokens.js
+++ NotificationsServer/functions/getDevicesTokens.js
@@ -3,17 +3,15 @@ var deviceTokens = require('../../API/models/deviceNotification')
 module.exports = async function getDevicesTokens(devicesList) {
 	console.log("tokens de devices")
 	var TokensList = []
-
-
 	await deviceTokens.find({}, async (err, deviceNot) => {
-		await devicesList.map( async (dev) => {
-			await deviceNot.map((devNot) => {
-				if (dev === devNot.refDevice){ 
+		//await devicesList.map( async (dev) => { //Recorre lista devices 
+			await deviceNot.map((devNot) => { // recorre lista de tokens 
+				//if (dev === devNot.refDevice){  // si son iguales los 
 					console.log("Device encontrado"+devNot.refDevice)
 					TokensList.push(devNot.fcmToken)
-				}
+				//}
 			})
-		})
+		//})
 	})
 	return TokensList
 }
